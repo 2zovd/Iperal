7b7bc48231e6aae2ffa111c9c3e4ce0283a26de7
DmytroT <tyzov@bk.ru>
2017-03-24 06:37:06 +0200

    v.1.0.1

--- js/common.js
+++ js/common.js
@@ -2,5 +2,10 @@
  $('.main-slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-  arrows: true
+  arrows: false,
+  autoplay: true
 });
+
+document.getElementById('nav-drop_btn').onclick = function() {
+	document.getElementById('nav-drop').classList.toggle('active-menu');
+}
